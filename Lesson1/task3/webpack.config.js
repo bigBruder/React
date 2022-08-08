536dc374f61722f10bbb93d4b508f82187480a4a
romontherock <romontherock@example.com>
2022-08-08 19:30:48 +0300

    firstjsxApp

---
+++ Lesson1/task3/webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
