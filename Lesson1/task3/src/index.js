29d653073a03d7c813c2f29211e4d2b93c1c399d
romontherock <romontherock@example.com>
2022-08-09 00:44:28 +0300

    firstcreatereactapp

--- Lesson1/task3/src/index.js
+++ Lesson1/task3/src/index.js
@@ -1,32 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
+import "./index.css";
 
 const rootElement = document.querySelector("#root");
 
 const element = (
-  <div className="greeting">
+  <div className="title">
     <div className="greeting__title">Hello, world!</div>
     <div className="greeting__text">I'm learning React</div>
   </div>
 );
 
 console.log(element);
-// const renderGreeting = (elem) => {
-//   const containerElem = document.createElement("div");
-//   containerElem.classList.add("greeting");
-
-//   const titleElem = document.createElement("h1");
-//   titleElem.classList.add("greeting__title");
-//   titleElem.textContent = "Hello, world!";
-
-//   const textElem = document.createElement("p");
-//   textElem.classList.add("greeting__text");
-//   textElem.textContent = "I'm learning React";
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
 
 ReactDOM.render(element, rootElement);
