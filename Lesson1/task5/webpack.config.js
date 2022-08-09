fe0f78afcababd0ea3d991207df0b06f44109dd4
romontherock <romontherock@example.com>
2022-08-09 16:13:56 +0300

    jsx

--- Lesson1/task5/webpack.config.js
+++ Lesson1/task5/webpack.config.js
@@ -1,26 +1,57 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "review_build"),
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        use: "babel-loader",
-      },
-      {
-        test: /.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const config = {
+    entry: "./src/index.js",
+    output: {
+      path: path.join(__dirname, "review_build"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /.s?css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /.(jpg|png|gif)/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 8192,
+                name: "[name].[ext]",
+                outputPath: "images",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+    devServer: {
+      port: 9000,
+      hot: true,
+    },
+  };
+  const productionPlugins = [new MiniCssExtractPlugin()];
+  if (isProduction) config.plugins.push(...productionPlugins);
+
+  return config;
 };
