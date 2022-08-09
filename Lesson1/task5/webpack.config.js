95d7e1425754800133eefd0be4b1949a89afb046
romontherock <romontherock@example.com>
2022-08-09 14:02:56 +0300

    jsx

---
+++ Lesson1/task5/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, "review_build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: "babel-loader",
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: __dirname,
+    inline: true,
+    host: "0.0.0.0",
+    port: 8080,
+  },
+};
