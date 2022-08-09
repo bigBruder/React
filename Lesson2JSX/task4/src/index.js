4de1a437fe7ac3ac4644ad2d65508f6b05e9b5be
romontherock <romontherock@example.com>
2022-08-10 00:54:23 +0300

    jsx

---
+++ Lesson2JSX/task4/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./list.css";
+
+const rootElement = document.querySelector("#root");
+
+const element = (
+  <main className="todo-list">
+    <div className="actions">
+      <input className="task-input" type="text" />
+      <button className="btn create-task-btn">Create</button>
+    </div>
+    <ul className="list"></ul>
+  </main>
+);
+
+ReactDOM.render(element, rootElement);
