e747bf70f11b11b10a7fd28d27eb89ad88d41df0
romontherock <romontherock@example.com>
2022-08-10 13:03:31 +0300

    vfskn

--- Lesson3ComponentsProps/task1/src/Search.jsx
+++ Lesson3ComponentsProps/task1/src/Search.jsx
@@ -1,5 +1,7 @@
-export default Search = () => {
-    return (
+import React from "react";
+
+const Search = () => {
+    return 
         <div className="search">
         <h1 className="search__title">Hello, Tom. What to search for you?</h1>
         <div className="search__field">
@@ -7,5 +9,7 @@ export default Search = () => {
             <button className="search__button">Search</button>
         </div>
     </div>
-    )
-    }
+
+    };
+
+    export default Search;
