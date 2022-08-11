3689c15e070a61f33a96f199b0c7b19fe2d4da31
romontherock <romontherock@example.com>
2022-08-11 13:48:40 +0300

    vfskn

--- Lesson3ComponentsProps/task4/src/Greeting.jsx
+++ Lesson3ComponentsProps/task4/src/Greeting.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import moment from "moment";
 
 const birthDate = newDate('2001-01-01T11:11:11.819z');
-const age = (newDate()-birthDate).getYears;
-comsole.log(age)
 
 export default function Greeting(props) {
   return (
