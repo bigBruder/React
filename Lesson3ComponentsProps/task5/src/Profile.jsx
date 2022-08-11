198979e4e4a70857f403c7d8097922c1c8c86385
romontherock <romontherock@example.com>
2022-08-11 15:04:55 +0300

    vfskn

--- Lesson3ComponentsProps/task5/src/Profile.jsx
+++ Lesson3ComponentsProps/task5/src/Profile.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import moment from "moment";
 
 export default function Profile(props) {
-  const data = props.birthDate;
-  const formatDate = data => moment(data).format("DD MMM YY");
+
+  const formatDate = date => moment(date).format("DD MMM YY");
   return (
     <>
     <div className="profile__name">
     `${props.firstName} ${props.lastName}`
 </div>
     <div className="profile__birth">
-    `Was born ${formatDate} in ${props.birthPlace}`
+    `Was born ${formatDate(props.date)} in ${props.birthPlace}`
 </div>
 </>
   );
