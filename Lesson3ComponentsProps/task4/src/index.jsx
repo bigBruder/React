d1c950d5525b06b3142f4c55853ced5388eb59eb
romontherock <romontherock@example.com>
2022-08-11 14:01:26 +0300

    vfskn

--- Lesson3ComponentsProps/task4/src/index.jsx
+++ Lesson3ComponentsProps/task4/src/index.jsx
@@ -4,7 +4,8 @@ import "./index.scss";
 import "./search.scss";
 import Greeting from './Greeting';
 
+// const birthDate = `${newDate('2001-01-01T11:11:11.819z')}`;
 
 const rootElement = document.querySelector("#root");
 
-ReactDOM.render(<Greeting firstName="John" lastName="Doe"/>, rootElement);
+ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate={17}/>, rootElement);
