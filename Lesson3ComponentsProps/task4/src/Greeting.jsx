d5887d02c8e3a7bcded4f7f053cde4d846449f77
romontherock <romontherock@example.com>
2022-08-11 18:07:03 +0300

    vfskn

--- Lesson3ComponentsProps/task4/src/Greeting.jsx
+++ Lesson3ComponentsProps/task4/src/Greeting.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import moment from "moment";
 
+// const formatDate = birthDate => moment(birthDate, "YYYYMMDD").from();
+
 export default function Greeting(props) {
   return (
     <div className="greeting">
-    `My name is ${props.firstName} ${props.lastName}. I'm 17 years old`
+    {`My name is ${props.firstName} ${props.lastName}. I'm ${props.birthDate} years old`}
 </div>
   );
 }
--- Lesson3ComponentsProps/task4/src/index.jsx
+++ Lesson3ComponentsProps/task4/src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.scss";
-import "./search.scss";
 import Greeting from './Greeting';
 
-// const birthDate = `${newDate('2001-01-01T11:11:11.819z')}`;
+
+// const birthDate = {birthDate:new Date('2001-01-01T11:11:11.819z')};
 
 const rootElement = document.querySelector("#root");
 
-ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate={17}/>, rootElement);
+ReactDOM.render(<Greeting firstName="John" lastName="Doe" birthDate={21}/>, rootElement);
