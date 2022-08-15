7d47b37e81eb7dc0d09baf88a1c64ddf2c385589
romontherock <romontherock@example.com>
2022-08-15 17:44:53 +0300

    react

---
+++ Lesson5/task6/src/ColorPicker.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+const CORAL = '#FF7F50';
+const BISQUE = '#FFE4C4';
+const AQUA = '#00FFFF';
+
+class ColorPicker extends Component {
+  setTitleColor = color => {
+    document.querySelector('.picker__title').textContent = color;
+  };
+  resetTitleColor = () => {
+    this.setState({
+      color: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="picker__title"></div>
+        <div>
+          <button
+            onMouseEnter={() => this.setTitleColor('Coral')}
+            onMouseLeave={() => this.resetTitleColor()}
+            className="picker__button picker__button_coral"
+          ></button>
+          <button
+            onMouseEnter={() => this.setTitleColor('Aqua')}
+            onMouseLeave={() => this.resetTitleColor()}
+            className="picker__button picker__button_aqua"
+          ></button>
+          <button
+            onMouseEnter={() => this.setTitleColor('Bisque')}
+            onMouseLeave={() => this.resetTitleColor()}
+            className="picker__button picker__button_bisque"
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
