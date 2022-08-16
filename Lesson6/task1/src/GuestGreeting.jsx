b2d226ea6eb0c8edf384557cfa607361ecd91fad
romontherock <romontherock@example.com>
2022-08-16 18:21:12 +0300

    react

--- Lesson6/task1/src/GuestGreeting.jsx
+++ Lesson6/task1/src/GuestGreeting.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const GuestGreeting = () => <h1>Hi Stranger. Please login🚀</h1>;
+const GuestGreeting = () => <h1>Hi stranger. Please login🚀</h1>;
 
 export default GuestGreeting;
--- Lesson6/task1/src/UserGreeting.jsx
+++ Lesson6/task1/src/UserGreeting.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const UserGreeting = () => <h1>Hello, User😎</h1>;
+const UserGreeting = () => <h1>Hello, user😎</h1>;
 
 export default UserGreeting;
