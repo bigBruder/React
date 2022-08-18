290ae35bdd3e96ad108bf74716cf3140e91392f9
romontherock <romontherock@example.com>
2022-08-18 13:29:18 +0300

    vfskn

--- Lesson7Lists/task3/src/Transaction.jsx
+++ Lesson7Lists/task3/src/Transaction.jsx
@@ -4,6 +4,8 @@ import moment from 'moment';
 // moment.locale(en-gb);
 
 const Transaction = ({ from, to, amount, rate, time}) => {
+  rate = new Intl.NumberFormat('en-GB').format(rate);
+  amount = new Intl.NumberFormat('en-GB').format(amount);
   return (
     <li className="transaction">
     <span className="transaction__date">{moment(time).format('ll')}</span>
