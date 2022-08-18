3dfb3cf38e474efab0ea2095c108efa1a3dd48e4
romontherock <romontherock@example.com>
2022-08-19 01:23:15 +0300

    lifecycleMethods

--- Lesson8LifecycleMethods/task2/src/Clock.jsx
+++ Lesson8LifecycleMethods/task2/src/Clock.jsx
@@ -1,38 +1,42 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './clock.scss';
-import moment from "moment";
+import moment from 'moment';
 
 const getTimeWithOffset = offset => {
-    const currentTime = new Date();
-    const utcOffset = currentTime.getTimezoneOffset() / 60;
-    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: props.location,
+      time: moment(getTimeWithOffset(props.offset)).format('LTS'),
+      offset: props.offset,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        time: moment(getTimeWithOffset(this.state.offset)).format('LTS'),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.state.location}</div>
+        <div className="clock__time">{this.state.time}</div>
+      </div>
+    );
+  }
 }
 
-// const formatDate = offsetData => moment(getTimeWithOffset(offsetData)).format('LTS')
-
-    class Clock extends Component{
-        constructor(props){
-            super(props);
-        this.state = {
-           location: props.location,
-           time: moment(getTimeWithOffset(props.offset)).format('LTS')
-          };
-        //   const time = moment(getTimeWithOffset(props.offset)).format('LTS');
-        //   console.log(time);
-          setInterval(()=>{
-            this.setState({
-                time: moment(getTimeWithOffset(props.offset)).format('LTS')
-            });
-          },1000)
-        }
-        render() {
-            return (
-                <div className="clock">
-                <div className="clock__location">{this.state.location}</div>
-                <div className="clock__time">{this.state.time}</div>
-                </div>
-            );
-        }
-    }
-
-    export default Clock;
+export default Clock;
