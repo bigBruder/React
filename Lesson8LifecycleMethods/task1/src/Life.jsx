75aed1300417aab7b40ea35f5b643834627d4395
romontherock <romontherock@example.com>
2022-08-19 22:28:34 +0300

    lifecycleMethods

--- Lesson8LifecycleMethods/task1/src/Life.jsx
+++ Lesson8LifecycleMethods/task1/src/Life.jsx
@@ -11,16 +11,16 @@ class Life extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate(nextProps, nextState):decide to render or not to render');
-    return nextProps;
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate(prevProps, prevState):some updates based on new props');
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount():cleanup before DOM related to component will be removed');
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
   }
 
   render() {
