48dd0d672fbe94c0cf9e948fb2c3b426c87e0346
romontherock <romontherock@example.com>
2022-08-19 17:58:02 +0300

    lifecycleMethods

--- Lesson8LifecycleMethods/task3/src/Dimensions.jsx
+++ Lesson8LifecycleMethods/task3/src/Dimensions.jsx
@@ -30,17 +30,6 @@ class Dimensions extends Component {
     document.title = `${innerWidth} x ${innerHeight}`;
   };
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log(nextProps, nextState);
-  //   console.log('shouldComponentUpdate(nextProps, nextState):decide to render or not to render');
-  //   return nextProps;
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   console.log(prevProps);
-  //   console.log('componentDidUpdate(prevProps, prevState):some updates based on new props');
-  // }
-
   render() {
     return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
   }
