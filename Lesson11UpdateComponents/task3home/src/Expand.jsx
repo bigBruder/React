522ddd5ea8048f22c2a203a16c9a3df3d56213a0
romontherock <romontherock@example.com>
2022-08-27 23:35:37 +0300

    reactUpdate

--- Lesson11UpdateComponents/task3home/src/Expand.jsx
+++ Lesson11UpdateComponents/task3home/src/Expand.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const Expand = ({ isOpen, children, title, onClick }) => (
-  <div className="expand border">
-    <div className="expand__header">
-      <span className="expand__title">{title}</span>
-      <button className="expand__toggle-btn" onClick={onClick}>
-        {isOpen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>}
-      </button>
+const Expand = ({ isOpen, children, title, onClick }) => {
+  let toggle;
+  if (isOpen) {
+    toggle = 'fas fa-chevron-up';
+  } else {
+    toggle = 'fas fa-chevron-down';
+  }
+  return (
+    <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">{title}</span>
+        <button className="expand__toggle-btn" onClick={onClick}>
+          <i className={toggle}></i>
+        </button>
+      </div>
+      {isOpen ? <div className="expand__content">{children}</div> : <div></div>}
     </div>
-    {isOpen ? <div className="expand__content">{children}</div> : <div></div>}
-  </div>
-);
-
+  );
+};
 export default Expand;
