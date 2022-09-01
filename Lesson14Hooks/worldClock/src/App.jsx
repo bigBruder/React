3d9cb4c4578462029520d32bb092498087ab20bc
romontherock <romontherock@example.com>
2022-09-01 13:54:25 +0300

    reactHooks

--- Lesson14Hooks/worldClock/src/App.jsx
+++ Lesson14Hooks/worldClock/src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Clock from './Clock';
 
 const App = () => {
-return (
+  return (
     <>
-<Clock location={'New York'} offset={-5}/>
-<Clock location={'Kyiv'} offset={2}/>
-<Clock location={'London'} offset={0}/>
-</>
-);
+      <Clock location={'New York'} offset={-5} />
+      <Clock location={'Kyiv'} offset={2} />
+      <Clock location={'London'} offset={0} />
+    </>
+  );
 };
 export default App;
--- Lesson14Hooks/worldClock/src/Clock.jsx
+++ Lesson14Hooks/worldClock/src/Clock.jsx
@@ -1,38 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 import './clock.scss';
-import moment from "moment";
+import moment from 'moment';
 
 const getTimeWithOffset = offset => {
-    const currentTime = new Date();
-    const utcOffset = currentTime.getTimezoneOffset() / 60;
-    return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
-}
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
 
 // const formatDate = offsetData => moment(getTimeWithOffset(offsetData)).format('LTS')
 
-    class Clock extends Component{
-        constructor(props){
-            super(props);
-        this.state = {
-           location: props.location,
-           time: moment(getTimeWithOffset(props.offset)).format('LTS')
-          };
-        //   const time = moment(getTimeWithOffset(props.offset)).format('LTS');
-        //   console.log(time);
-          setInterval(()=>{
-            this.setState({
-                time: moment(getTimeWithOffset(props.offset)).format('LTS')
-            });
-          },1000)
-        }
-        render() {
-            return (
-                <div className="clock">
-                <div className="clock__location">{this.state.location}</div>
-                <div className="clock__time">{this.state.time}</div>
-                </div>
-            );
-        }
-    }
+const Clock = props => {
+  const [state, setState] = useState({
+    location: props.location,
+    time: moment(getTimeWithOffset(props.offset)).format('LTS'),
+  });
 
-    export default Clock;
+  //   const time = moment(getTimeWithOffset(props.offset)).format('LTS');
+  //   console.log(time);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setState({
+        time: moment(getTimeWithOffset(props.offset)).format('LTS'),
+        location: props.location,
+      });
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  const { location, time } = state;
+  return (
+    <div className="clock">
+      <div className="clock__location">{location}</div>
+      <div className="clock__time">{time}</div>
+    </div>
+  );
+};
+
+export default Clock;
