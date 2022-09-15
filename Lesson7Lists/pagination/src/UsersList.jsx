9e13c2a2777bda8c606c3a79af1e59293c10743b
romontherock <romontherock@example.com>
2022-09-15 12:25:08 +0300

    pagination

--- Lesson7Lists/pagination/src/UsersList.jsx
+++ Lesson7Lists/pagination/src/UsersList.jsx
@@ -1,44 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import User from './User';
 import Pagination from './Pagination';
 
-const UsersList = ({users}) => { 
+const UsersList = ({ users }) => {
+  const [state, setState] = useState({
+    currentPage: 1,
+    currentFirstIndex: 0,
+    currentSecondIndex: 3,
+    currentUsers: users.slice(0, 3),
+  });
 
-const [state, setState] = useState({currentPage: 1, currentFirstIndex:0, currentSecondIndex:3, currentUsers: users.slice(0,3)})
+  const { currentUsers, currentPage, currentFirstIndex, currentSecondIndex } = state;
 
-const { currentUsers, currentPage, currentFirstIndex, currentSecondIndex } = state;
-
-const goPrev = () => {
-  const a = currentFirstIndex-3;
-  const b = currentSecondIndex-3;
+  const goPrev = () => {
+    const a = currentFirstIndex - 3;
+    const b = currentSecondIndex - 3;
     setState({
-      currentFirstIndex:a,
-      currentSecondIndex:b,
-      currentUsers: users.slice(a,b),
-      currentPage: currentPage - 1
-    })
-}
+      currentFirstIndex: a,
+      currentSecondIndex: b,
+      currentUsers: users.slice(a, b),
+      currentPage: currentPage - 1,
+    });
+  };
 
-const goNext = () => {
-  const a = currentFirstIndex+3;
-  const b = currentSecondIndex+3;
+  const goNext = () => {
+    const a = currentFirstIndex + 3;
+    const b = currentSecondIndex + 3;
     setState({
-      currentFirstIndex:a,
-      currentSecondIndex:b,
-      currentUsers: users.slice(a,b),
-      currentPage: currentPage + 1
-    })
-}
+      currentFirstIndex: a,
+      currentSecondIndex: b,
+      currentUsers: users.slice(a, b),
+      currentPage: currentPage + 1,
+    });
+  };
 
-        return (
-            <div className='users'>
-                {<Pagination goPrev={goPrev} 
-                goNext={goNext} 
-                currentPage={currentPage} 
-                totalItems={users.length} />}
-                {currentUsers.map((user)=>(<User key={user.id} name={user.name} age={user.age}/>))}
-            </div>
-        );
-    }
+  return (
+    <div className="users">
+      {
+        <Pagination
+          goPrev={goPrev}
+          goNext={goNext}
+          currentPage={currentPage}
+          totalItems={users.length}
+          itemsPerPage={itemsPerPage}
+        />
+      }
+      {currentUsers.map(user => (
+        <User key={user.id} name={user.name} age={user.age} />
+      ))}
+    </div>
+  );
+};
 
 export default UsersList;
